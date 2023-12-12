8adbc05ad6d12f7ac68ecc2bbe6535ecc97cf2ca
Marta Barnolas <soterasbm@gmail.com>
2023-12-12 10:28:29 +0100

    initial commit

---
+++ typescript-project/index.ts
@@ -0,0 +1,34 @@
+function obtenerChiste () {
+  const options = {
+    method: 'GET',
+    headers : {
+      'Accept' : 'application/json'
+    }
+
+  }
+  fetch ('https://icanhazdadjoke.com', options)
+  .then((res) => res.json())
+  .then(response => console.log(response.joke));
+    }
+
+
+obtenerChiste();
+
+function otroChiste (){
+
+  const options = {
+    method: 'GET',
+    headers : {
+      'Accept' : 'application/json'
+    }
+
+  }
+  fetch ('https://icanhazdadjoke.com', options)
+  .then((res) => res.json())
+  .then(response => {
+    let chisteElement = document.getElementById('acudit');
+    chisteElement.innerHTML = `<h1>${response.joke}</h1>`;
+})
+
+}
+
